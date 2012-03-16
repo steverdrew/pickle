4f82b08af3a936a3d1e636f07efe8a2a0e81c99d
Steve Drew <steve@cloudaltitude.com>
2012-03-16 16:04:20 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickle::Application.config.secret_token = 'aee3116d18a91cbd6d9d04d260e5d937d4469aa2d3eef0a0b640771e34444a1e41c1fb044bc537b55620229a64bda670e08cf0a282af9a529a435c001816f6fe'
