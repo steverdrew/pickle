6959b7e360333b39ccd9b4f7592821123d937f69
Steve Drew <steve@cloudaltitude.com>
2012-03-20 10:58:42 +0000

    js update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,9 @@
 //= require jquery-1.3.2.min.js
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-//= require jquery.dualSlider.0.3
-//= require jquery.dualSlider.0.3.min
 //= require jquery.easing.1.3.js
 //= require jquery.timers-1.2.js
+//= require jquery.dualSlider.0.3.min
+//= require_tree .
+
+
