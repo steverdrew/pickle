76c0f4a31b5b34667e38e2cc07164cb6c02c1f37
Steve Drew <steve@cloudaltitude.com>
2012-03-20 10:19:52 +0000

    new carousel, half way there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
+//= require jquery.dualSlider.0.3
+//= require jquery.dualSlider.0.3.min
+//= require jquery.easing.1.3.js
+//= require jquery.timers-1.2.js
+//= require jquery-1.3.2.min.js
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
